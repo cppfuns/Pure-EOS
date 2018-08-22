a59ca0a8f9558ead5c10c62cc3e8b074c666a20c
suweigang <suweigang@kingsoft.com>
2018-08-22 21:23:19 +0800

    change sql_db

---
+++ plugins/sql_db_plugin/include/eosio/sql_db_plugin/table.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <memory>
+#include <soci/soci.h>
+#include<soci/mysql/soci-mysql.h>
+#include <fc/io/json.hpp>
+#include <fc/variant.hpp>
+#include <fc/time.hpp>
+
+namespace eosio{
+
+class mysql_table{
+    public:
+
+        void reconnect(std::shared_ptr<soci::session> m_session){
+            m_session->reconnect();
+        }
+
+        fc::microseconds max_serialization_time = fc::microseconds(150*1000);
+
+};
+
+}
